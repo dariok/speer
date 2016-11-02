e357d7a72ff539e1b947cc769daf4206594f05d5
Dario Kampkaspar <dariok@web.de>
2016-11-02 17:47:42 +0100

    JS zur Wortsuche hinzugefügt

---
+++ script/word.js
@@ -0,0 +1,21 @@
+function ladeDRW (suchbegriff) {
+  suchbegriff = escape(suchbegriff);
+  breite = screen.availWidth;
+  hoehe = screen.availHeight;
+  if (breite > 800) {
+  	breite = 800;
+  }
+  drw=window.open("http://drw-www.adw.uni-heidelberg.de/drw-cgi/metasuche_ext?execterm="+suchbegriff,"drw","width="+breite+",height="+hoehe+",left=0,top=0,scrollbars=yes,resizable=yes,toolbar=yes");
+  drw.focus();
+}
+
+function zeige () {
+  //vgl. http://de.selfhtml.org/navigation/suche/index.htm?Suchanfrage=selectionStart
+  if (window.getSelection) {
+  ladeDRW(window.getSelection());
+  } else if (document.getSelection) {
+  ladeDRW(document.getSelection());
+  } else if (document.selection) {
+  ladeDRW(document.selection.createRange().text);
+  }
+}
